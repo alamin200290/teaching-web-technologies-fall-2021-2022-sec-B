c0a5752fee2580193863485f5653bb05987f31fd
sarwar_sikder <amin.se.aiub@gmail.com>
2021-10-12 13:30:29 +0600

    multi user login system

--- login_with_file_system/loginCheck.php
+++ login_with_file_system/loginCheck.php
@@ -7,19 +7,19 @@
 	
 		if($username != ""){
 			if($password != ""){
-			
-				$myfile = fopen('user.txt', 'r');				
-				$data = fread($myfile, filesize('user.txt'));
-				//fgets();
-				//feof();
-				//while
-				$myuser = explode('|', $data);
-				if(trim($myuser[0]) == $username && trim($myuser[1]) == $password){
-					setcookie('flag', 'true', time()+3600, '/');
-					header('location: home.php');
-				}else{
-					echo "invalid username/password...";
+				$myfile = fopen('user.txt', 'r');
+
+				while(!feof($myfile)){
+					$data = fgets($myfile);
+					$myuser = explode('|', $data);
+					if(trim($myuser[0]) == $username && trim($myuser[1]) == $password){
+						setcookie('flag', 'true', time()+3600, '/');
+						header('location: home.php');
+					}
 				}
+
+				echo "invalid username/password...";
+					
 
 			}else{
 				echo "invalid password...";
